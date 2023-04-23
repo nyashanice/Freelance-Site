0cb616ec6a7bea563877f214a26392988a2b9a78
Nya Tyus <nyastyus@gmail.com>
2023-04-23 01:28:48 -0400

    add bio to about me section

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/About.css";
 
-export default function About(){
-    return (<div>about</div>)
+export default function About() {
+  return (
+    <div>
+      <div></div>
+      <div className="body">
+        <div className="p-2">
+          My name is Nya and I'm an Atlanta based web developer. My coding
+          journey started late in 2022 and I graduated from Georgia Institute of
+          Technology's full stack coding bootcamp in March 2023.
+        </div>
+        <div className="p-2">
+          I have always had a love for STEM and I graduated with a B.S. in
+          biology in December 2022. I grew to love coding because of the freedom
+          it gives me to be creative and continue learning. It allows me to
+          challenge myself which I also appreciate. Being in a position to help
+          others showcase their talents and abilities brings me joy.
+        </div>
+        <div className="p-2">
+          During the bootcamp, I learned laguages and used libraries such as
+          MERN stack (MongoDB, Express.js, ReactJS, Node.js), HTML5, CSS3,
+          JavaScript ES6, MySQL, NoSQL, API integration, and object oriented
+          programming.
+        </div>
+        <div className="p-2">
+          In my free time I enjoy creating content on YouTube, Instagram, and
+          TikTok, cooking, reading, and most of all: traveling!
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Main.css";
+import About from "./About";
+import Work from "./Work";
+import Services from "./Services";
+import Booking from "./Booking";
 
 export default function Main() {
   return (
-    <div className="main p-5">
-      <div className="title text-center">Hey! My name is Nya Shanice.</div>
-      <div className="subtitle text-center">
-        web di-'ve-lə-pər : creating websites for service based businesses
+    <div>
+      <div className="main p-5">
+        <div className="title text-center">Hey! My name is Nya Shanice.</div>
+        <div className="subtitle text-center">
+          web developer (web di-'ve-lə-pər) : creating websites for service
+          based businesses
+        </div>
       </div>
+      <About />
+      <Work />
+      <Services />
+      <Booking />
     </div>
   );
 }
