5ffcf72ab9bede6b2eb9bb65b599d2d544de5eee
Nya Tyus <nyastyus@gmail.com>
2023-04-23 00:47:05 -0400

    style introductory section

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/Main.css";
 
 export default function Main() {
-  return <div>main</div>;
+  return (
+    <div className="main p-5">
+      <div className="title text-center">Hey! My name is Nya Shanice.</div>
+      <div className="subtitle text-center">
+        web di-'ve-lə-pər : creating websites for service based businesses
+      </div>
+    </div>
+  );
 }
