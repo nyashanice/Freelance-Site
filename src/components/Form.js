70f5b7b3dbd4531345d7a5d837afd680624b7a8e
Nya Tyus <nyastyus@gmail.com>
2023-05-14 17:09:08 -0400

    resize iframe form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,67 @@
 import React from "react";
 import { Widget } from "@typeform/embed-react";
+import IframeResizer from "iframe-resizer-react";
+import { useEffect } from "react";
 
 export default function Form() {
+  useEffect(() => {
+    let hecticFrame = document.getElementById("hectic-form");
+    hecticFrame.src =
+      "https://hello.withmoxie.com/01/the-faithful-pixel/inquiry-form?inFrame=true&sourceUrl=" +
+      encodeURIComponent(window.location.href);
+  }, []);
+
+  // useEffect(() => {
+  //   setTimeout(
+  //     () =>
+  //       IframeResizer(
+  //         {
+  //           heightCalculationMethod: "min",
+  //           sizeWidth: true,
+  //           sizeHeight: true,
+  //           log: false,
+  //           checkOrigin: false,
+  //         },
+  //         "#hectic-form"
+  //       ),
+  //     100
+  //   );
+  // }, []);
+
+  const timeout = (e) => {
+    e.preventDefault();
+    setTimeout(() => {
+      console.log("timeout");
+    }, 10000);
+  };
+
   return (
-    <Widget
-      id="avcH5PZM"
-      style={{ width: "100%", height: "700px" }}
-      className="my-form"
-    />
+    // <Widget
+    //   id="avcH5PZM"
+    //   style={{ width: "100%", height: "700px" }}
+    //   className="my-form"
+    // />
+    <div>
+      <script
+        type="text/javascript"
+        src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.3.2/iframeResizer.min.js"
+      />
+      <div style={{ width: "100vw", height: "100vh" }}>
+        <iframe
+          id="hectic-form"
+          title="title"
+          allowtransparency="true"
+          style={{ padding: "0px", margin: "0px", border: "0", width: "100%", height: '100%' }}
+        ></iframe>
+        <IframeResizer
+          onLoad={timeout}
+          heightCalculationMethod="max"
+          sizeWidth="true"
+          sizeHeight="true"
+          log="false"
+          checkOrigin="false"
+        />
+      </div>
+    </div>
   );
 }
